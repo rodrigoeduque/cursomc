532edbc26af0049dd27f2e9c8aa44ecbb730ea7b
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-08-30 10:54:09 -0300

    Banco de Dados H2

--- src/main/java/br/com/rodrigoeduque/cursomcnelioalves/categorias/model/Categoria.java
+++ src/main/java/br/com/rodrigoeduque/cursomcnelioalves/categorias/model/Categoria.java
@@ -1,9 +1,16 @@
 package br.com.rodrigoeduque.cursomcnelioalves.categorias.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
 
