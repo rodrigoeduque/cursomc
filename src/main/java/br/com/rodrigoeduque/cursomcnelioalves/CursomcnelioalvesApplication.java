5b0ac83a7904a8fac4200e6643e69769643a3b01
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-08-30 09:45:06 -0300

    Projeto Criado

---
+++ src/main/java/br/com/rodrigoeduque/cursomcnelioalves/CursomcnelioalvesApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.cursomcnelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcnelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcnelioalvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoeduque/cursomcnelioalves/CursomcnelioalvesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.cursomcnelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcnelioalvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
