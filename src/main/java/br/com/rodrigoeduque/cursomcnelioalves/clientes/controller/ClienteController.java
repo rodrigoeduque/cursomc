d12924b72eb2e756e55bc4bf66be60b5d0e846a4
Rodrigo Eustaquio Duque <rodrigoeduque@hotmail.com>
2021-08-31 15:09:50 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/rodrigoeduque/cursomcnelioalves/clientes/controller/ClienteController.java
@@ -0,0 +1,2 @@
+package br.com.rodrigoeduque.cursomcnelioalves.clientes.controller;public class ClienteController {
+}
---
+++ src/main/java/br/com/rodrigoeduque/cursomcnelioalves/clientes/service/ClienteService.java
@@ -0,0 +1,2 @@
+package br.com.rodrigoeduque.cursomcnelioalves.clientes.service;public class ClienteService {
+}
